6067fa9386482f01d949a9d03fc50dcf4e2c951b
RichardG <richard.gross@maibornwolff.de>
2017-01-14 22:17:55 +0100

    made database more of an interface

--- infrastructure/src/main/kotlin/de/constantinuous/dietrich/data/Database.kt
+++ infrastructure/src/main/kotlin/de/constantinuous/dietrich/data/Database.kt
@@ -18,97 +18,52 @@ class Database (private val dataSource: DataSource) {
     fun getStoredProcedures() {
         val connection = getDBConnection()
 
-        val stmt = connection.createStatement();
-        stmt.execute("""
-        CREATE ALIAS QUERY AS $$
-        ResultSet query(Connection conn, String sql) throws SQLException {
-          return conn.createStatement().executeQuery(sql);
-        } $$;
-        """)
-        stmt.close()
-
         val procedures = connection.metaData.getProcedures(null, null, "%")
 
         while (procedures.next()) {
             println("Procedure: "+procedures.getString(3))
         }
 
-         connection.prepareCall("CALL QUERY('SELECT * FROM DUAL');").use{ cStmt ->
-             var hadResults = cStmt.execute();
-             while (hadResults) {
-                 val rs = cStmt.resultSet;
 
-                 println(rs)
-                 hadResults = cStmt.moreResults;
-             }
-         }
     }
 
+
     @Throws(SQLException::class)
-    fun insertWithPreparedStatement() {
+    fun call(sql: String) {
         val connection = getDBConnection()
+        connection.prepareCall(sql).use{ cStmt ->
+            var hadResults = cStmt.execute();
+            while (hadResults) {
+                val rs = cStmt.resultSet;
 
-        val CreateQuery = "CREATE TABLE PERSON(id int primary key, name varchar(255))"
-        val InsertQuery = "INSERT INTO PERSON" + "(id, name) values" + "(?,?)"
-        val SelectQuery = "select * from PERSON"
-
-        try {
-            connection.setAutoCommit(false)
-
-            val createPreparedStatement = connection.prepareStatement(CreateQuery)
-            createPreparedStatement.executeUpdate()
-            createPreparedStatement!!.close()
-
-            val insertPreparedStatement = connection.prepareStatement(InsertQuery)
-            insertPreparedStatement.setInt(1, 1)
-            insertPreparedStatement.setString(2, "Jose")
-            insertPreparedStatement.executeUpdate()
-            insertPreparedStatement.close()
-
-            val selectPreparedStatement = connection.prepareStatement(SelectQuery)
-            val rs = selectPreparedStatement.executeQuery()
-            println("H2 In-Memory Database inserted through PreparedStatement")
-            while (rs.next()) {
-                System.out.println("Id " + rs.getInt("id") + " Name " + rs.getString("name"))
+                println(rs)
+                hadResults = cStmt.moreResults;
             }
-            selectPreparedStatement.close()
-
-            connection.commit()
-        } catch (e: SQLException) {
-            System.out.println("Exception Message " + e.message)
-        } catch (e: Exception) {
-            e.printStackTrace()
-        } finally {
-            connection.close()
         }
     }
 
     @Throws(SQLException::class)
-    fun insertWithStatement() {
-        val connection = getDBConnection()
-        try {
+    fun executeSql(sql: String) {
+        getDBConnection().use { connection ->
             connection.setAutoCommit(false)
-            val stmt = connection.createStatement()
-            stmt.execute("CREATE TABLE PERSON(id int primary key, name varchar(255))")
-            stmt.execute("INSERT INTO PERSON(id, name) VALUES(1, 'Anju')")
-            stmt.execute("INSERT INTO PERSON(id, name) VALUES(2, 'Sonia')")
-            stmt.execute("INSERT INTO PERSON(id, name) VALUES(3, 'Asha')")
-
-            val rs = stmt.executeQuery("select * from PERSON")
-            println("H2 In-Memory Database inserted through Statement")
-            while (rs.next()) {
-                System.out.println("Id " + rs.getInt("id") + " Name " + rs.getString("name"))
+            connection.createStatement().use { statement ->
+                statement.execute(sql)
             }
 
-            stmt.execute("DROP TABLE PERSON")
-            stmt.close()
-            connection.commit()
-        } catch (e: SQLException) {
-            System.out.println("Exception Message " + e.message)
-        } catch (e: Exception) {
-            e.printStackTrace()
-        } finally {
-            connection.close()
+        }
+    }
+
+    @Throws(SQLException::class)
+    fun executeQuery(sql: String) {
+        getDBConnection().use { connection ->
+            connection.setAutoCommit(false)
+            connection.createStatement().use { statement ->
+                val result = statement.executeQuery(sql)
+                while (result.next()) {
+                    System.out.println("Id " + result.getInt("id") + " Name " + result.getString("name"))
+                }
+            }
+
         }
     }
 
--- infrastructure/src/test/kotlin/de/constantinuous/dietrich/data/DatabaseTest.kt
+++ infrastructure/src/test/kotlin/de/constantinuous/dietrich/data/DatabaseTest.kt
@@ -12,17 +12,49 @@ class DatabaseTest : FeatureSpec(){
         feature("Empty Feature") {
             scenario("Test statement insert"){
                 val foo = Database(DriverManagerDataSource())
-                foo.insertWithStatement()
-            }
 
-            scenario("Test prepared statement insert"){
-                val foo = Database(DriverManagerDataSource())
-                foo.insertWithPreparedStatement()
+                foo.executeSql("CREATE TABLE PERSON(id int primary key, name varchar(255))")
+                foo.executeSql("INSERT INTO PERSON(id, name) VALUES(1, 'Anju')")
+                foo.executeSql("INSERT INTO PERSON(id, name) VALUES(2, 'Sonia')")
+                foo.executeSql("INSERT INTO PERSON(id, name) VALUES(3, 'Asha')")
+
+                foo.executeQuery("SELECT * FROM PERSON")
+
+                foo.executeSql("DROP ALL OBJECTS")
             }
 
             scenario("Test getting stored procedures"){
                 val foo = Database(DriverManagerDataSource())
+                foo.executeSql("""
+                CREATE ALIAS QUERY AS $$
+                ResultSet query(Connection conn, String sql) throws SQLException {
+                  return conn.createStatement().executeQuery(sql);
+                } $$;
+                """)
+
                 foo.getStoredProcedures()
+
+                foo.call("CALL QUERY('SELECT * FROM DUAL');")
+
+                foo.executeSql("DROP ALL OBJECTS")
+            }
+
+            scenario("Test from stored procedure query table"){
+                val foo = Database(DriverManagerDataSource())
+                foo.executeSql("""
+                CREATE ALIAS QUERY AS $$
+                ResultSet query(Connection conn, String sql) throws SQLException {
+                  return conn.createStatement().executeQuery(sql);
+                } $$;
+                """)
+                foo.executeSql("CREATE TABLE DUAL(id int primary key, name varchar(255))")
+                foo.executeSql("INSERT INTO DUAL(id, name) VALUES(1, 'Anju')")
+                foo.executeSql("INSERT INTO DUAL(id, name) VALUES(2, 'Sonia')")
+                foo.executeSql("INSERT INTO DUAL(id, name) VALUES(3, 'Asha')")
+
+                foo.call("CALL QUERY('SELECT * FROM DUAL');")
+
+                foo.executeSql("DROP ALL OBJECTS")
             }
         }
     }
